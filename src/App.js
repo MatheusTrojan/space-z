adea01fc916ec5147c2c12255bdd213909f77f06
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-06 20:52:34 -0300

    cria rotas iniciais com o BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import InitialPage from "./pages/InitialPage"
+import About from "./pages/About"
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/" element={ <InitialPage/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="*" element={ <h1>Página não encontrada!</h1> } />
+      </Routes>
     </BrowserRouter>
   );
 }
